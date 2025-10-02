ddda5d1c57879c1afafe321d16c93eec0c8aa4bd
tcs-moe-inoue <moe.inoue@tcs.com>
2025-10-02 16:03:20 +0900

    レイアウト初期化

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex flex-col min-h-screen text-center text-gray-800">
+      {/* ヘッダー */}
+      <header className="bg-blue-300 text-white py-3 px-4 shadow">
+        <h1 className="text-xl font-bold">Shopping List App</h1>
+      </header>
+
+      {/* メイン */}
+      <main className="flex-1 p-6">
+        <p className="text-base">ここに入力フォームやリストが入る予定</p>
+      </main>
+
+      {/* フッター */}
+      <footer className="bg-gray-800 text-white text-center">
+        <small className="text-[10px]">&copy; 2025 Shopping App</small>
+      </footer>
+    </div>
   );
 }
 
