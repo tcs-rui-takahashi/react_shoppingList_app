68cb7d1397195da6bc7c163667e2e67173309714
tcs-moe-inoue <moe.inoue@tcs.com>
2025-10-03 11:59:37 +0900

    コメントを削除

--- src/types/Items.ts
+++ src/types/Items.ts
@@ -1,10 +1,10 @@
 export interface Item {
-  id: string;             // 一意のID
-  name: string;           // 品名
-  quantity?: number;      // 数量（任意）
-  unit?: string;          // 単位（例：個、箱など）（任意）
-  completed: boolean;     // 購入済みかどうか
-  plannedDate?: Date;     // 購入予定日（任意）
-  purchaseDate?: Date;    // 実際に購入した日（任意）
-  note?: string;          // メモ（任意）
+  id: string;             
+  name: string;           
+  quantity?: number;      
+  unit?: string;          
+  completed: boolean;     
+  plannedDate?: Date;     
+  purchaseDate?: Date;    
+  note?: string;       
 }
