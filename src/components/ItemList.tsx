79b029c1b6cde59834e39630f9b63a054e495d21
tcs-moe-inoue <moe.inoue@tcs.com>
2025-10-06 13:39:05 +0900

    Listの後の不要な空白を削除

--- src/components/ItemList.tsx
+++ src/components/ItemList.tsx
@@ -3,7 +3,7 @@ import { Item } from './Item';
 export function ItemList() {
   return (
     <section className="border p-4 rounded mb-4">
-      <h2 className="font-bold text-lg mb-2">Item List </h2>
+      <h2 className="font-bold text-lg mb-2">Item List</h2>
       <Item />
       <Item />
       <Item />
